fffaff2a5046e012aea9affd1783b0759702cf55
masanmu <467877331@qq.com>
2016-05-27 12:32:13 +0800

    change core/__init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -35,7 +35,7 @@ class Open_Falcon_Api(object):
                 response = self.opener.open(method_url,urllib.urlencode(self.request_data))
             else:
                 response = self.opener.open(method_url)
-            print response.read()      
+            print "ok" 
         except Exception as e:
             print "Error:",e
     def __getattr__(self,method):
